e13328102a9fce0f5e5aa780c4e64d85d4866147
Jordan Lee <jordanl.7890@gmail.com>
2025-04-21 11:43:31 -0700

    allow all option

--- scripts/euc_distance.sh
+++ scripts/euc_distance.sh
@@ -2,6 +2,15 @@
 set -e
 cancer=${1}
 prep_path=${2}
-
-mkdir -p data/distance_metric/${cancer}_GEXP
-python scripts/dist_model-tumor.py --cancer PAAD --prep_path ${prep_path}
+# Run all cohorts
+if [[ ${cancer} == 'ALL' ]]; then
+  declare -a StringArray=('BLCA' 'BRCA' 'COADREAD' 'ESO' 'KIRCKICH' 'LGGGBM' 'LIHCCHOL' 'LUNG' 'OV' 'PAAD' 'SARC' 'SKCM' 'UCEC')
+  for cancer in ${StringArray[@]}; do
+    mkdir -p data/distance_metric/${cancer}_GEXP
+    python scripts/dist_model-tumor.py --cancer PAAD --prep_path ${prep_path}
+    done
+# Run single cohort
+else
+  mkdir -p data/distance_metric/${cancer}_GEXP
+  python scripts/dist_model-tumor.py --cancer PAAD --prep_path ${prep_path}
+fi
